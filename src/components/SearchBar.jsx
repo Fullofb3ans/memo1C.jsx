8b40439e1dccbfaa87ac6f26045847b062e0f53a
Fullofb3ans <igorgazinskii6@gmail.com>
2024-02-27 16:35:33 +0400

    патч

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,7 +6,7 @@ export function SearchBar() {
 			<Row>
 				<Col sm={12}>
 					<Form className="d-flex">
-						<Form.Control type="search" placeholder="Search" className="me-2 rounded-pill" aria-label="Search" />
+						<Form.Control type="search" placeholder="Поиск" className="me-2 rounded-pill" aria-label="Search" />
 						<Button className="rounded-pill" variant="outline-primary">
 							Поиск
 						</Button>
